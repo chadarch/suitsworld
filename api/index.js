460cbe5228b60e985225cf0e2a0869440a20193d
Sage Developer <dev@sagefoundation.com>
2025-07-09 03:13:20 +0200

    backend cors server fix 2

--- api/index.js
+++ api/index.js
@@ -38,6 +38,17 @@ const connectDB = async () => {
 // Create Express app
 const app = express();
 
+// Manual CORS middleware for debugging - add this at the very top
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // For debugging only!
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
+
 // Middleware
 app.use(cors({
   origin: (origin, callback) => {
